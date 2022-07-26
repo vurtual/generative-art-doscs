6656526622139df99b53f25cda3af4c431334e43
Andrew Sharpe <andrew@sharpe-thinking.tech>
2022-07-26 09:06:38 +0100

    performance improvements

--- GraphicObject.js
+++ GraphicObject.js
@@ -4,7 +4,7 @@ const GraphicObject = object => {
   const graphicObject = object
   graphicObjects.push(graphicObject)
 
-  graphicObject.remove = () => {
+  graphicObject._remove = () => {
     graphicObjects.splice(indexOf(graphicObject), 1)
   }
   return graphicObject
--- Tile.js
+++ Tile.js
@@ -1,46 +1,46 @@
-const Tile = (x, y, size, row, column, rowCount, columnCount) => {
-  const tile = {}
-  tile.pos = { x, y }
-  tile.origin = { x: x + size / 2, y: y }
-  tile.size = size
-  tile.row = row
-  tile.column = column
-  tile.type = 'Tile'
-  tile.rotate = { x: 0, y: 0, z: 0 }
-  tile.opacity = 200
-  tile.color = { r: random(100, 200), g: random(100, 200), b: random(100, 200) }
-
-  tile.update = () => {
-    const d = distance(tile.pos, { x: mouseX, y: mouseY })
-    tile.rotate.x += (deltaTime * d) / width / 10000
-    tile.rotate.y += (deltaTime * d) / height / 1000
-    // tile.opacity = map(d, 0, width / 2, 255, 155)
+function Tile(x, y, size, row, column, rowCount, columnCount) {
+  this.pos = { x, y }
+  this.origin = { x: x + size / 2, y: y }
+  this.size = size
+  this.row = row
+  this.column = column
+  this.type = 'Tile'
+  this._rotate = { x: 0, y: 0, z: 0 }
+  this.opacity = 200
+  this._color = {
+    r: random(100, 200),
+    g: random(100, 200),
+    b: random(100, 200),
   }
+}
 
-  tile.draw = () => {
-    push()
-    translate(tile.origin.x, tile.origin.y)
-    fill(tile.color.r, tile.color.g, tile.color.b, tile.opacity)
-    // noStroke()
-    rotateX(tile.rotate.x)
-    rotateY(tile.rotate.y)
-    ellipse(-tile.size, -tile.size, tile.size, tile.size)
-    pop()
-  }
+Tile.prototype.update = function () {
+  const d = distance(this.pos, { x: mouseX, y: mouseY })
+  this._rotate.x += (deltaTime * d) / width / 10000
+  this._rotate.y += (deltaTime * d) / height / 1000
+  return this
+}
 
-  GraphicObject(tile)
-  return tile
+Tile.prototype.draw = function () {
+  push()
+  translate(this.origin.x, this.origin.y)
+  fill(this._color.r, this._color.g, this._color.b, this.opacity)
+  // noStroke()
+  rotateX(this._rotate.x)
+  rotateY(this._rotate.y)
+  rect(-this.size, -this.size, this.size, this.size)
+  pop()
 }
 
 const generateTiles = () => {
-  const w = (width - (width % 50)) / 50
+  const tilesPerRow = 35
+  const w = (width - (width % tilesPerRow)) / tilesPerRow
   const h = w
   const columnCount = (width - (width % w)) / w
   const rowCount = (height - (height % h)) / h
-  console.log(columnCount, rowCount)
   for (let i = 0; i < columnCount; i++) {
     for (let j = 0; j < rowCount; j++) {
-      Tile(
+      const tile = new Tile(
         w + i * w + 2 - width / 2 + ((i + 1) * (width % w)) / columnCount,
         w + j * w + 2 - height / 2 + ((j + 1) * (height % h)) / rowCount,
         w * 2,
@@ -49,6 +49,7 @@ const generateTiles = () => {
         rowCount,
         columnCount
       )
+      GraphicObject(tile)
     }
   }
 }
